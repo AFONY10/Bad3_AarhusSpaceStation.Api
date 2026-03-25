25fea8f372c4ecaefe46febc15e64b80c8d8533e
AFONY10 <133364236+AFONY10@users.noreply.github.com>
2026-03-25 13:41:56 +0100

    Migratios work and database update works with updated connectionstring

---
+++ AarhusSpaceProgram.Api/Migrations/20260325115248_InitialCreate.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AarhusSpaceProgram.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Astronauts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HoursInSpace = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Astronauts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CelestialBodies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CelestialBodies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Launchpads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Launchpads", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Managers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Managers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rockets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    Manufacturer = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rockets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scientists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FieldOfExpertise = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scientists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Missions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    LaunchDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManagerId = table.Column<int>(type: "int", nullable: true),
+                    RocketId = table.Column<int>(type: "int", nullable: true),
+                    LaunchpadId = table.Column<int>(type: "int", nullable: true),
+                    TargetCelestialBodyId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Missions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Missions_CelestialBodies_TargetCelestialBodyId",
+                        column: x => x.TargetCelestialBodyId,
+                        principalTable: "CelestialBodies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Missions_Launchpads_LaunchpadId",
+                        column: x => x.LaunchpadId,
+                        principalTable: "Launchpads",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Missions_Managers_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "Managers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Missions_Rockets_RocketId",
+                        column: x => x.RocketId,
+                        principalTable: "Rockets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MissionAstronauts",
+                columns: table => new
+                {
+                    AstronautsId = table.Column<int>(type: "int", nullable: false),
+                    MissionsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionAstronauts", x => new { x.AstronautsId, x.MissionsId });
+                    table.ForeignKey(
+                        name: "FK_MissionAstronauts_Astronauts_AstronautsId",
+                        column: x => x.AstronautsId,
+                        principalTable: "Astronauts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MissionAstronauts_Missions_MissionsId",
+                        column: x => x.MissionsId,
+                        principalTable: "Missions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MissionScientists",
+                columns: table => new
+                {
+                    MissionsId = table.Column<int>(type: "int", nullable: false),
+                    ScientistsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionScientists", x => new { x.MissionsId, x.ScientistsId });
+                    table.ForeignKey(
+                        name: "FK_MissionScientists_Missions_MissionsId",
+                        column: x => x.MissionsId,
+                        principalTable: "Missions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MissionScientists_Scientists_ScientistsId",
+                        column: x => x.ScientistsId,
+                        principalTable: "Scientists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissionAstronauts_MissionsId",
+                table: "MissionAstronauts",
+                column: "MissionsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_LaunchpadId_LaunchDate",
+                table: "Missions",
+                columns: new[] { "LaunchpadId", "LaunchDate" },
+                unique: true,
+                filter: "[LaunchpadId] IS NOT NULL AND [LaunchDate] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_ManagerId",
+                table: "Missions",
+                column: "ManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_RocketId",
+                table: "Missions",
+                column: "RocketId",
+                unique: true,
+                filter: "[RocketId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_TargetCelestialBodyId",
+                table: "Missions",
+                column: "TargetCelestialBodyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissionScientists_ScientistsId",
+                table: "MissionScientists",
+                column: "ScientistsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MissionAstronauts");
+
+            migrationBuilder.DropTable(
+                name: "MissionScientists");
+
+            migrationBuilder.DropTable(
+                name: "Astronauts");
+
+            migrationBuilder.DropTable(
+                name: "Missions");
+
+            migrationBuilder.DropTable(
+                name: "Scientists");
+
+            migrationBuilder.DropTable(
+                name: "CelestialBodies");
+
+            migrationBuilder.DropTable(
+                name: "Launchpads");
+
+            migrationBuilder.DropTable(
+                name: "Managers");
+
+            migrationBuilder.DropTable(
+                name: "Rockets");
+        }
+    }
+}
